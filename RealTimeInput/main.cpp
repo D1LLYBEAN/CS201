208bfeec7b826cf40ad16e1962c7f7ef8946f302
Dillon <ninjamills@hotmail.com>
2017-09-13 15:17:00 -0800

    Made a Real-Time Input test.
    Being able to read keystrokes in real time has a lot of potential functionality (especially with games), so I made this project. It appears to work, but I've only tried reading ESC. Function keys, and some other special keys, read as really strange codes, so I'm not yet sure if they work.

---
+++ RealTimeInput/main.cpp
@@ -0,0 +1,39 @@
+// main.cpp
+// RealTimeInput
+// Dillon Mills
+// Created 9/13/17
+// Reads key presses in real time, and exits when ESC is pressed.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <conio.h>
+
+
+// Main program
+// Input an integer with error checking.
+int main()
+{
+    cout << "Enter ESC at any point to exit!" << endl;
+    int key;
+    bool loop = true;
+    while (loop)
+    {
+        key = _getch();
+        if (key == 0)
+        {
+            key = getch(); // retrieve second byte of extended code
+            key *= 256; // shift left by 8 bits to get extended code
+        }
+        switch (key)
+        {
+            case 27: // ESC
+                cout << endl << "Exiting..." << endl;
+                loop = false;
+                break;
+            default:
+                break;
+        }
+    }
+    cout << endl << "End of Program!" << endl;
+}
