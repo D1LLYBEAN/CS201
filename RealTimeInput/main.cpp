3483c40c2792a60b00ddb9db9f28202cd3d0e6ec
Dillon <ninjamills@hotmail.com>
2017-10-23 10:03:21 -0800

    Completed Lab21, Improved RTI
    Lab 21 was about pointers.
    
    I made it so the player's "avatar" changes to a different arrow symbol, depending on the direction moved.
    Can't print left arrow for some reason.
    Will porbably make it so turning is done independently of moving.

--- RealTimeInput/main.cpp
+++ RealTimeInput/main.cpp
@@ -14,12 +14,43 @@ using std::vector;
 #include <string>
 using std::string;
 
+void printMap(int pPosX, int pPosY, char avatar)
+{
+    string outputString = "#" + string(16,'#') + "#\n#";
+    for(int i = 15; i >= 0; i--)
+    {
+        for(int j = 0; j <= 15; j++)
+        {
+            if (pPosX == i && pPosY == j)
+            {
+                outputString += avatar;
+            }
+            else
+            {
+                outputString += " ";
+            }
+        }
+        outputString += "#\n#";
+    }
+    outputString += string(16,'#') + "#\n";
+
+    system("CLS");
+    cout << outputString;
+}
+
 
 int main()
 {
     vector<int> pPos(2);
+    char pAvat = '\x18';
 
-    cout << "Press ESC at any point to exit!" << endl;
+    printMap(pPos[0],pPos[1],pAvat);
+    cout << "\nWelcome to my game!\n";
+    cout << "There's not much to it yet, but it's getting better.\n";
+    cout << "\nYour character is the \"i\" symbol.\n";
+    cout << "Move around with WASD.\n";
+    cout << "\nPress ESC at any point to exit.\n";
+    cout << "I'll understand...\n";
     int key;
     bool loop = true;
     while (loop)
@@ -38,15 +69,19 @@ int main()
         {
             case 'W':
                 pPos[0] += (pPos[0] < 15)? 1 : 0;
+                pAvat = '\x18';
                 break;
             case 'A':
                 pPos[1] -= (pPos[1] > 0)? 1 : 0;
+                pAvat = '\x1a'; // can't get it to print left arrow!!!
                 break;
             case 'S':
                 pPos[0] -= (pPos[0] > 0)? 1 : 0;
+                pAvat = '\x19';
                 break;
             case 'D':
                 pPos[1] += (pPos[1] < 15)? 1 : 0;
+                pAvat = '\x1a';
                 break;
             case 27: // ESC
                 cout << endl << "Exiting..." << endl;
@@ -57,28 +92,11 @@ int main()
                 break;
         }
 
-        string outputString = "#" + string(16,'#') + "#\n#";
-        for(int i = 15; i >= 0; i--)
-        {
-            for(int j = 0; j <= 15; j++)
-            {
-                if (pPos[0] == i && pPos[1] == j)
-                {
-                    outputString += "i";
-                }
-                else
-                {
-                    outputString += " ";
-                }
-            }
-            outputString += "#\n#";
-        }
-        outputString += string(16,'#') + "#\n";
-
-        system("CLS");
-        cout << outputString;
+        printMap(pPos[0],pPos[1],pAvat);
     }
 
     system("CLS");
-    cout << endl << "End of Program!" << endl;
+    cout << endl << "Goodbye!" << endl;
+
+    return 0;
 }
